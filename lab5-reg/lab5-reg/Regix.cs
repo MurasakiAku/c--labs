3e40217c7823164f34a37ef32483d204d20ea6a3
MurasakiAku <Nastya-13-14@yandex.ru>
2023-02-25 16:23:18 +0700

    5
    5

---
+++ lab5-reg/lab5-reg/Regix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace lab5_reg
+{
+    class Regex
+    {
+        private string v;
+
+        public Regex(string v)
+        {
+            this.v = v;
+        }
+
+        internal static bool IsMatch( string str)
+        {
+            return Regex.IsMatch(str);
+        }
+    }
+}
