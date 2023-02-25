70f918d840ad1f39d1ff37d8a141b7e0a9f119f7
MurasakiAku <Nastya-13-14@yandex.ru>
2023-02-25 16:23:48 +0700

    2
    2

---
+++ Regex.v2/Regex.v2/regexx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regexxx.v2
+{
+    class regexx
+    {
+        //Regex Regex = new Regex("@[a-z,A-Z,0-9], {5,}");
+        
+        //string s = Console.ReadLine();
+        //Console.WriteLine("");
+    }
+}
