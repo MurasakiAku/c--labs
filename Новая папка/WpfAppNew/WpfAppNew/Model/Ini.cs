b8cc4ac6b9a9dc1f6150d48942bd4cc1afb01a5a
MurasakiAku <Nastya-13-14@yandex.ru>
2023-04-02 11:04:56 +0700

    ye

--- Новая папка/WpfAppNew/WpfAppNew/Model/Ini.cs
+++ Новая папка/WpfAppNew/WpfAppNew/Model/Ini.cs
@@ -11,24 +11,50 @@ namespace WpfAppNew.Model
     {
         protected override void Seed(MarketModel db)
         {
-            Seller s1 = new Seller()
+            Seller seller1 = new Seller()
             {
+                Id = 1,
                 FirstName = "Иван",
                 LastName = "Иванович",
                 Fatherland = "Иванов"
             };
-            Seller s2 = new Seller()
+            Seller seller2 = new Seller()
             {
+                Id = 2,
                 FirstName = "Карнаков",
                 LastName = "Матвей",
                 Fatherland = "Александрович"
             };
-            SellerCategory category = new
+            SellerCategory category1 = new SellerCategory()
+            {
+                Id = 1,
+                SellerCategories = "Ученик"
+            };
+            SellerCategory category2 = new SellerCategory()
+            {
+                Id = 2,
+                SellerCategories = "Кассир"
+            };
+            SellerCategory category3 = new SellerCategory()
+            {
+                Id = 3,
+                SellerCategories = "Старший кассир"
+            };
 
-            Cash cash = new Cash()
+            Cash cash1 = new Cash()
             {
                 Id = 1,
                 CashNumber = 1
+            };
+            Cash cash2 = new Cash()
+            {
+                Id = 2,
+                CashNumber = 2
+            };
+            Sale sale1 = new Sale()
+            {
+                Id = 1,
+               // DateTime = ""
             };
 
         }
