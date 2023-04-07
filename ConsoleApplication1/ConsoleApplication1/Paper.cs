d4a9c58c079b76f7cb8ade3fd5120847a1ad9468
MurasakiAku <Nastya-13-14@yandex.ru>
2023-04-07 20:03:58 +0700

    new

--- ConsoleApplication1/ConsoleApplication1/Paper.cs
+++ ConsoleApplication1/ConsoleApplication1/Paper.cs
@@ -7,44 +7,30 @@ namespace ConsoleApplication1
 {
     class Paper
     {
-        public string _Publication;
-        public Person _person;
-        public DateTime _PublicationDate;
+        public string Publication { get; set; }
+        public Person Person { get; set; }
+        public DateTime PublicationDate { get; set; }
 
         public Paper(string Publication, Person person, DateTime PublicationDate)
         {
-            _Publication = Publication;
-            _person = person;
-            _PublicationDate = PublicationDate;
+            Publication = Publication;
+            Person = Person;
+            PublicationDate = PublicationDate;
         }
         public Paper()
         {
-            _Publication = "Bendy";
-            _person = new Person();
-            _PublicationDate = new DateTime(1935, 06, 09);
+            Publication = "Bendy";
+            Person = new Person();
+            PublicationDate = new DateTime(1935, 06, 09);
         }
-        public string publication
-        {
-            get { return _Publication; }
-            set { _Publication = value; }
-        }
-        public Person persons
-        {
-            get { return _person; }
-            set { _person = value; }
-        }
-        public DateTime publicationdate
-        {
-            get { return _PublicationDate; }
-            set { _PublicationDate = value; }
-        }
+        
         public override string ToString()
         {
-            return _Publication + ' ' + _person + ' ' + _PublicationDate.ToString("dd.mm.yyyy");
+            return Publication + ' ' + Person + ' ' + PublicationDate.ToString("dd.mm.yyyy");
         }
         public string ToFullString()
         {
-            return $"Наименоввание публикации: {_Publication}, Автор публикации: {_person}, Дата публикации: {_PublicationDate}";
+            return $"Наименоввание публикации: {Publication}, Автор публикации: {Person}, Дата публикации: {PublicationDate}";
         }
     }
 }
--- ConsoleApplication1/ConsoleApplication1/Person.cs
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -7,50 +7,37 @@ namespace ConsoleApplication1
 {
     class Person
     {
-        private string First_Name;
-        private string Last_Name;
-        private DateTime BDate;
-        public Person(string _First_Name, string _Last_Name, DateTime _BDate)
-        {
-            First_Name = _First_Name;
-            Last_Name = _Last_Name;
-            BDate = _BDate;
-        }
+        private string _firstname { get; set; }
+        private string _lastname { get; set; }
+        private DateTime _bdate { get; set; }
 
+        public Person(string FirstName, string LastName, DateTime BDate)
+        {
+            _firstname = FirstName;
+            _lastname = LastName;
+            _bdate = BDate;
+        }
+        
         public Person()
         {
-            First_Name = "Joye";
-            Last_Name = "Drew";
-            BDate = new DateTime(1930, 01, 01);
+            _firstname = "Joye";
+            _lastname = "Drew";
+            _bdate = new DateTime(1930, 01, 01);
         }
-        public string Name
-        {
-            get { return First_Name; }
-            set { First_Name = value; }
-        }
-        public string LName
-        {
-            get { return Last_Name; }
-            set { Last_Name = value; }
-        }
-        public DateTime HappyBri
-        {
-            get { return BDate; }
-            set { BDate = value; }
-        }
-        public int year
-        {
-            get { return BDate.Year; }
-            set { year = value; }
-        }
+        
+        //public int year
+        //{
+        //    get { return BDate.Year; }
+        //    set { year = value; }
+        //}
         public string ToFullString()
         {
-            return $"Имя: {First_Name}, фамилия: {Last_Name}, дата рождения: {BDate}";
+            return $"Имя: {_firstname}, фамилия: {_lastname}, дата рождения: {_bdate}";
         }
 
         public string ToShortString()
         {
-            return $"Имя: {First_Name}, фамилия: {Last_Name}";
+            return $"Имя: {_firstname}, фамилия: {_lastname}";
         }
     }   
 
